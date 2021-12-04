062fd763b54d8de5ed54f7e8d2c378ea390fcb9c
lauraminkova <laura.minkova@yahoo.ca>
2021-12-04 02:02:07 +0100

    updates on features and installed packages

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # INF554-Final-Project
+
+Downloaded BERT from:  https://github.com/google-research/bert
+
+Downloaded SciBERT from:https://github.com/allenai/scibert
--- textual_learning/features.py
+++ textual_learning/features.py
@@ -5,6 +5,10 @@
 import numpy as np
 import pandas as pd
 import os
+from transformers import AutoTokenizer, AutoModel
+import nltk
+from nltk.corpus import stopwords
+nltk.download("stopwords") 
 
 def number_papers(ls_authors, authors_paper) :
     '''
@@ -16,7 +20,7 @@ def number_papers(ls_authors, authors_paper) :
     
     for a in ls_authors.index :
         author = str(ls_authors.loc[a])
-        ls_num_papers = 5 - authors_paper.loc[author].isna().sum().sum()
+        ls_num_papers[a - min(ls_authors.index)] = 5 - authors_paper.loc[author].isna().sum().sum()
     
     return ls_num_papers
 
@@ -29,19 +33,81 @@ def total_sum_abs_words(ls_authors, authors_paper, abstracts) :
     
     ls_length = [0] * ls_authors.shape[0]
 
-    for a in ls_authors.index :
-        author = str(ls_authors.loc[a])
+    for a in range(ls_authors.shape[0]) :
+        author = str(ls_authors.iloc[a])
         for pid in authors_paper.loc[author] :
             # Checking paperID exists, and that it's abstract is available to us
             if (pid != None) and (len(abstracts.loc[abstracts.PID == pid, 'InvInd'].values) != 0):
                 inv_ind = eval(abstracts.loc[abstracts.PID == pid, 'InvInd'].values[0])
-                ls_length[a] += inv_ind['IndexLength'] 
+                ls_length[a - min(ls_authors.index)] += inv_ind['IndexLength'] 
     
     return ls_length
 
+def get_scibert_vectors(ls_authors, authors_paper, abstracts) :
+    '''
+    ls_authors:     (Serie-like) list of the authorIDs of interest
+    authors_paper:  (DataFrame) dataframe with authorIDs and their corresponding paperIDs
+    abstracts:      (DataFrame) dataframe with paperIDs and their string abstracts (modified abstracts.txt)
+    '''
+  
+    # Loading SciBERT
+    tokenizer = AutoTokenizer.from_pretrained("allenai/scibert_scivocab_uncased")
+    model = AutoModel.from_pretrained("allenai/scibert_scivocab_uncased")
 
-TEXT_FEATURES = {'numpapers': number_papers, 'tot_abs_len': total_sum_abs_words}
+    # Loading stop words
+    stop_words = set(stopwords.words("english"))
+
+    ls_vectors = [0] * ls_authors.shape[0]
+
+    for a in range(ls_authors.shape[0]) :
+
+        # This part concatenates all of the abstracts of an author
+        author = str(int(ls_authors.iloc[a].values[0]))
+        all_abstracts = ""
+        for pid in authors_paper.loc[author] :
+            if (pid != None) and (len(abstracts[abstracts.PID == pid]['InvertedIndex'].values) != 0):
+                abs = abstracts[abstracts.PID == pid]['InvertedIndex'].values[0]
+                all_abstracts += abs + " "
+        
+        # Removing stop words
+        filtered_list = [word for word in all_abstracts.split() if word.casefold() not in stop_words]
+        all_abs_no_stopwrd = (" ").join(filtered_list)
+        
+        # This part makes the word embeddings for the abstracts
+        inputs = tokenizer(all_abs_no_stopwrd, return_tensors="pt")
+        if(inputs['input_ids'].shape[1] > 512) :
+            inputs['input_ids'] = inputs['input_ids'][:, :512]
+            inputs['token_type_ids'] = inputs['token_type_ids'][:, :512]
+            inputs['attention_mask'] = inputs['attention_mask'][:, :512]
+        outputs = model(**inputs)
+        tens = outputs.pooler_output.detach().numpy()
+        tens = tens.reshape(tens.shape[1])
+        ls_vectors[a - min(ls_authors.index)] = tens
+    
+    return ls_vectors
 
 
-if __name__ == '__main__' :
-    print("hi")
+import pickle 
+
+if __name__ == "__main__" :
+
+    # text data
+    new_abs = pd.read_parquet('data/new_abstracts.parquet')
+
+    author_papers = open("data/author_papers.txt", "r")
+    author_papers = author_papers.readlines()
+    author_papers = pd.DataFrame(author_papers,columns=['Col'])
+    author_papers[['author','Papers']] = author_papers['Col'].str.split(':',expand=True)
+    author_papers[['P1','P2','P3','P4','P5']] = author_papers['Papers'].str.split('-',expand=True)
+    author_papers = author_papers.replace({'\n':''}, regex=True)
+    author_papers.index = author_papers['author']
+    author_papers.drop(columns = ['Col', 'Papers', 'author'], axis = 1, inplace = True)
+
+    training = pd.read_csv('data/train.csv', dtype={'author': np.int64, 'hindex': np.float32})
+    X_train_10 = training[:10]
+
+    ls_vectors = get_scibert_vectors(training[:10], author_papers, new_abs)
+    X_train_10['vec'] = ls_vectors
+
+    with open("wrd_test4.pkl", "wb") as g :
+        pickle.dump(ls_vectors, g)
