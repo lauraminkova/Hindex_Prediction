831c053b5b428cc7d7268b2e7fd157995f8e94ad
lauraminkova <laura.minkova@yahoo.ca>
2021-12-07 00:17:40 +0100

    jotted down some ideas

--- TODO.md
+++ TODO.md
@@ -3,5 +3,8 @@ To do:
 * ~~have folder for text methods and another for graph methods~~
 * ~~have one script for complete pipeline~~
 * handle the unbalanced-ness of the data
+* when cross-validating, make sure the folds are "even" in the sense that they are
+equally inbalanced
 * graph loss while training
 * Use statistical approaches to verify that our features are important (p test)
+* use Chi-Sq from assessment on data given to improve predictions?
--- classifiers/classify.py
+++ classifiers/classify.py
@@ -2,3 +2,24 @@
 #   A script that stores the functions for all the different    #
 #               classifiers that can be used.                   #
 # ------------------------------------------------------------- #
+
+# Will return MLP model (untrained)
+def MLP(params) :
+
+    return 0
+
+def LASSO(params) :
+
+    return 0
+
+def RF(params) :
+
+    return 0
+
+def DecisionTreeRegressor(params) :
+
+    return 0
+
+def RF(params) :
+
+    return 0
