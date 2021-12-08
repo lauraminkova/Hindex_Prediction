87454683834bff03f9cfc437c3ec466cbbe149a8
lauraminkova <laura.minkova@yahoo.ca>
2021-12-08 15:30:58 +0100

    fixing my last mistake

--- features/graph_features.py
+++ features/graph_features.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 import numpy as np
 import networkx as nx
+import nodevectors
 from sklearn.cluster import DBSCAN
 from node2vec import Node2Vec
 
@@ -56,7 +57,7 @@ def all_networkx_feats(edgelist) :
         pickle.dump(pagerank, f)
         
         
-def graph_clustering(embedding_with_author) :
+def DBSCAN_graph_clustering(embedding_with_author) :
     '''
     embedding_with_author:      (DataFrame) DataFrame with authorID and embeddings
     '''
@@ -76,9 +77,14 @@ def graph_clustering(embedding_with_author) :
     author_with_cluster.to_parquet('graph_clustering.parquet')
 
     return author_with_cluster
-    
 
-def node2vec_emb(G, dimensions=5, walk_length=3, num_walks=50, workers=1)
+
+def node2vec_emb(G, dimensions=5, walk_length=3, num_walks=50, workers=1) :
+    '''
+    G:      (networkx graph) The graph corresponding to coauthorship.edgelist
+
+    The rest of the parameters are parameters for Node2Vec, please see its documentation for details.
+    '''
 
     # Precompute probabilities and generate walks - 
     node2vec = Node2Vec(G, dimensions=5, walk_length=3, num_walks=50, workers=1)  
@@ -91,3 +97,14 @@ def node2vec_emb(G, dimensions=5, walk_length=3, num_walks=50, workers=1)
 
     # Save model for later use
     model.save('model')
+
+def prone_emb(G) :
+    '''
+    G:      (networkx graph) The graph corresponding to coauthorship.edgelist
+    '''
+
+    prone_model = nodevectors.ProNE() 
+    embeddings = prone_model.fit_transform(G)
+
+    with open("PRONE_embedding.pkl", "wb") as f :
+        pickle.dump(embeddings, f)
